69da69246a3a033322d4bacf9cc32e7c6927c7a6
Pontus Pohl <pontuspohl@dhcp-232-171.nomad.chalmers.se>
2015-10-12 14:27:38 +0200

    main.java

---
+++ src/com/team9/bookingsystem/Main.java
@@ -0,0 +1,14 @@
+package com.team9.bookingsystem;
+
+/**
+ * Created by pontuspohl on 12/10/15.
+ */
+public class Main {
+
+    public static void main( String[] args){
+
+
+        System.out.println("Working");
+
+    }
+}
