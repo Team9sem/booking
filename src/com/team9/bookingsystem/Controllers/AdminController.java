86438aeda9a01570e994d2ff8250f82bd692c881
Nima Fard <nimafard@dhcp-228-090.nomad.chalmers.se>
2015-11-17 12:02:48 +0100

    Admin controller stuff

--- src/com/team9/bookingsystem/Controllers/AdminController.java
+++ src/com/team9/bookingsystem/Controllers/AdminController.java
@@ -20,7 +20,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Callback;
@@ -36,6 +35,8 @@ public class AdminController {
     // ArrayList Storing the most recent searchResult;
     private ArrayList<Room> searchResult;
     private Room selectedRoom;
+    private ToggleGroup toggleGroup;
+    private Object selectedButton;
 
  // ContainerElements
     @FXML AnchorPane topAnchorPane;
@@ -43,22 +44,15 @@ public class AdminController {
     @FXML BorderPane borderPane;
     @FXML HBox paginationBox;
     @FXML AnchorPane resultAnchorPane;
-    
-    
+    @FXML GridPane searchOptions;
+    @FXML Label loginLabel;
     @FXML Label searchPreferences;
-    @FXML Label searchForUser;
-    @FXML Label adminRoomLabel;
-    @FXML TextField userTextField; 
-    @FXML TextField roomTextField;
+    @FXML Label searchFor;
+    @FXML ToggleButton userToggle;
+    @FXML ToggleButton roomToggle;
+
 //    @FXML Label features;
-    @FXML RadioButton radioID;
-    @FXML RadioButton radioUserName;
-    @FXML RadioButton radioName;
-    @FXML RadioButton radioType;
-    @FXML RadioButton radioPnumber;
-    @FXML RadioButton radioRoomID;
-    @FXML RadioButton radioSomething;
-    @FXML Button adminSearchButton;
+
     
   
 //    @FXML Label location;
@@ -71,6 +65,9 @@ public class AdminController {
 //        setupDatePicker();
     	util = new MysqlUtil();
         paginationBox.setAlignment(Pos.CENTER);
+        userToggle.setToggleGroup(toggleGroup);
+        roomToggle.setToggleGroup(toggleGroup);
+        
 //        ObservableList<String> choices= FXCollections.observableArrayList();
 //        choices.addAll("OneChoice");
 //        locationPick.setItems(choices);
@@ -84,206 +81,42 @@ public class AdminController {
 
     }
     
-    public Pagination initPagination(){
-
-        Pagination pagination = new Pagination();
-        pagination.getStyleClass().add("pagination");
-
-
-        pagination.setPageFactory(new Callback<Integer, Node>() {
-            @Override
-            public Node call(Integer PageIndex) {
-
-                System.out.println("Callback runs");
-                VBox vBox = createPage(PageIndex);
-
-
-
-                return vBox;
-            }
-
-        });
-        return pagination;
-    }
-    
     public VBox createPage(int pageIndex){
 
         VBox vBox = new VBox(5);
 
         vBox.setAlignment(Pos.CENTER);
-        final ToggleGroup group = new ToggleGroup();
-
-        group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+        
+        GridPane grid =new GridPane();
+ 
+       
+        toggleGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
             @Override
             public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
                 if(newValue!=null){
-
-                    selectedRoom = (Room)group.getSelectedToggle().getUserData();
-                    System.out.println(selectedRoom.toString());
+                	
+//               
+                    selectedButton = toggleGroup.getSelectedToggle();
+                    System.out.println(selectedButton.toString());
                 }
             }
         });
-
-        int page = pageIndex * getElementsPerPage();
-        for(int i=page;i < page + getElementsPerPage();i++){
-            if(i < searchResult.size()){
-
-                HBox element = new HBox();
-
-                element.setStyle("-fx-background-color: rgba(48, 57, 88, 0.25);" +
-                        " -fx-background-radius: 15px; -fx-border-color: black; -fx-border-radius: 15px;");
-
-
-
-                GridPane gridPane = new GridPane();
-                gridPane.setHgap(10);
-
-
-
-                HBox locationElement = new HBox();
-                HBox sizeElement = new HBox();
-                HBox buttonElement = new HBox();
-
-
-                locationElement.setAlignment(Pos.CENTER_LEFT);
-                sizeElement.setAlignment(Pos.CENTER_LEFT);
-                buttonElement.setAlignment(Pos.CENTER);
-
-                locationElement.setPrefWidth(200);
-                sizeElement.setPrefWidth(150);
-                buttonElement.setPrefWidth(150);
-                locationElement.setPrefHeight(100);
-                sizeElement.setPrefHeight(100);
-                buttonElement.setPrefHeight(100);
-
-
-
-                gridPane.setMargin(locationElement,new Insets(0.0,0.0,0.0,15.0));
-
-
-                Label location = new Label("Room in "+searchResult.get(i).getLocation());
-
-                Label size = new Label("Size of room is: "+searchResult.get(i).getRoomSize());
-
-                location.getStyleClass().add("result-element-label");
-                size.getStyleClass().add("result-element-label");
-
-                locationElement.getChildren().add(location);
-                sizeElement.getChildren().add(size);
-                //
-
-                ToggleButton button = new ToggleButton("Select Room");
-                button.setUserData(searchResult.get(i));
-                button.setTextAlignment(TextAlignment.LEFT);
-                button.setAlignment(Pos.CENTER);
-                button.setToggleGroup(group);
-                button.getStyleClass().add("element-toggle-button");
-                buttonElement.getChildren().add(button);
-
-
-                gridPane.add(locationElement,0,0);
-                gridPane.add(sizeElement,1,0);
-                gridPane.add(buttonElement,2,0);
-
-
-                element.getChildren().add(gridPane);
-//                content.getChildren().add(button);
-//                content.setAlignment(Pos.CENTER);
-//                content.setPrefWidth(250);
-//                content.getStyleClass().add("result-box");
-
-                element.setAlignment(Pos.CENTER);
-                vBox.getChildren().add(element);
-                vBox.setVgrow(element, Priority.ALWAYS);
-
-            }
-            else{
-                HBox element = new HBox();
-                Region region = new Region();
-                element.getChildren().add(region);
-                element.setAlignment(Pos.CENTER);
-                element.setHgrow(region,Priority.ALWAYS);
-                vBox.getChildren().add(element);
-                vBox.setVgrow(element, Priority.ALWAYS);
-            }
-
+        
+        return vBox;
 
         }
-        return vBox;
-    }
+
+    
     
     private int getElementsPerPage(){
         return 5;
     }
    
-    @FXML public void Search(ActionEvent event) {
-        System.out.println("searching");
-
-
-//        Format Hours
-//        String fromHour = formatHour(fromTimeInput.getLocalTime().getHour());
-//        String toHour = formatHour(toTimeInput.getLocalTime().getHour());
-//
-//
-//        System.out.println(fromHour+" : "+toHour);
-
-
-//        System.out.println(toString());
-          FindRoomService findRoomService = new FindRoomService(new Task() {
-              @Override
-              protected Object call() throws Exception {
-                  return null;
-              }
-          });
-
-
-                  findRoomService.start();
-        findRoomService.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-
-                searchResult = (ArrayList<Room>) findRoomService.getValue();
-
-                if (searchResult != null) {
-
-                    Pagination pagination = initPagination();
-
-
-                    System.out.println(searchResult.size());
-                    if (searchResult.size() <= 5) {
-                        System.out.println("if happened");
-
-                        pagination.setPageCount(1);
-                        pagination.setCurrentPageIndex(0);
-                        paginationBox.getChildren().clear();
-                        paginationBox.getChildren().add(pagination);
-
-
-                    } else {
-                        pagination.setPageCount((int) (Math.ceil(searchResult.size() / 5.0)));
-                        pagination.setCurrentPageIndex(0);
-                        paginationBox.getChildren().clear();
-                        paginationBox.getChildren().add(pagination);
-                    }
-    			}
-                else {
-                    System.out.println("no result");
-                }
-
-
-            }
-        });
-        findRoomService.setOnFailed(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-                searchResult = new ArrayList<Room>();
-                Pagination pagination = initPagination();
-                pagination.setPageCount(1);
-                pagination.setCurrentPageIndex(0);
-                paginationBox.getChildren().clear();
-                paginationBox.getChildren().add(pagination);
-            }
-        });
+    @FXML public void getUserSearch(ActionEvent event){
+    	
+    	if (selectedButton == userToggle) {
+    		
+    	}
+    	
     }
-
 }
