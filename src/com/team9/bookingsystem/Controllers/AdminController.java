717677ee02020d973961ceba21e1ae16d5244f1a
Pontus Pohl <pontuspohl@dhcp-230-245.nomad.chalmers.se>
2015-11-18 15:24:21 +0100

    started big merge

--- src/com/team9/bookingsystem/Controllers/AdminController.java
+++ src/com/team9/bookingsystem/Controllers/AdminController.java
@@ -16,6 +16,8 @@ import com.team9.bookingsystem.Threading.User.FindRoomService;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
@@ -43,6 +45,12 @@ public class AdminController{
     private Room selectedRoom;
     private ToggleGroup toggleGroup;
     private Object selectedButton;
+    private ArrayList<Room> searchResult;
+    private ObservableList<Room> tableViewData = FXCollections.observableArrayList();
+    private ArrayList<Room> updatedRooms = new ArrayList<>();
+    private TableView<Room> table = new TableView<>();
+
+
 
  // ContainerElements
     @FXML private AnchorPane topAnchorPane;
@@ -98,6 +106,188 @@ public class AdminController{
 
             }
         });
+
+
+    }
+
+
+    private void setupTableView(){
+
+        TableColumn roomid = new TableColumn("ID");
+
+        roomid.setCellValueFactory(new PropertyValueFactory<Room,Integer>("roomID"));
+        roomid.setPrefWidth(10.0);
+
+
+        TableColumn location = new TableColumn("Location");
+        location.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        location.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Room,String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Room,String> event) {
+
+                Room updatedRoom = event.getTableView().getItems().get(event.getTablePosition()
+                        .getRow());
+                updatedRoom.setLocation(event.getNewValue());
+                if(!updatedRooms.contains(updatedRoom)){
+                    System.out.println("in first if,\n this is updatedRooms");
+                    updatedRooms.add(updatedRoom);
+                    System.out.println(updatedRooms.toString());
+                }
+                else{
+                    System.out.println("in else,\n this is updatedRooms");
+                    System.out.println(updatedRooms.toString());
+                }
+                System.out.println("this is tableview ArrayList:");
+                System.out.println(tableViewData.toString());
+            }
+        });
+
+        location.setCellValueFactory(new PropertyValueFactory<Room,String>("location"));
+        location.setPrefWidth(30.0);
+
+        TableColumn size = new TableColumn("Size");
+        size.setCellValueFactory(new PropertyValueFactory<Room,String>("roomSize"));
+        size.setPrefWidth(10.0);
+
+
+        TableColumn projector = new TableColumn("Projector");
+        projector.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        projector.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Room,Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Room,Integer> event) {
+
+                if(event.getNewValue() == 1 || event.getNewValue() == 0)
+                {
+                    Room updatedRoom = event.getTableView().getItems().get(event.getTablePosition()
+                            .getRow());
+                    updatedRoom.setHasProjector(event.getNewValue());
+                    if(!updatedRooms.contains(updatedRoom)){
+                        System.out.println("in first if,\n this is updatedRooms");
+                        updatedRooms.add(updatedRoom);
+                        System.out.println(updatedRooms.toString());
+                    }
+                    else{
+                        System.out.println("in else,\n this is updatedRooms");
+                        System.out.println(updatedRooms.toString());
+                    }
+                    System.out.println("this is tableview ArrayList:");
+                    System.out.println(tableViewData.toString());
+                }
+                else{
+                    event.getTableColumn().setVisible(false);
+                    event.getTableColumn().setVisible(true);
+
+
+                }
+            }
+        });
+
+        projector.setCellValueFactory(new PropertyValueFactory<Room,Integer>("hasProjector"));
+        projector.setPrefWidth(10.0);
+
+        TableColumn whiteboard = new TableColumn("WhiteBoard");
+        whiteboard.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        whiteboard.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Room,Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Room,Integer> event) {
+
+                if(event.getNewValue() == 1 || event.getNewValue() == 0){
+
+
+
+                    Room updatedRoom = event.getTableView().getItems().get(event.getTablePosition()
+                            .getRow());
+                    updatedRoom.setHasWhiteboard(event.getNewValue());
+                    if(!updatedRooms.contains(updatedRoom)){
+                        System.out.println("in first if,\n this is updatedRooms");
+                        updatedRooms.add(updatedRoom);
+                        System.out.println(updatedRooms.toString());
+                    }
+                    else{
+                        System.out.println("in else,\n this is updatedRooms");
+                        System.out.println(updatedRooms.toString());
+                    }
+                    System.out.println("this is tableview ArrayList:");
+                    System.out.println(tableViewData.toString());
+                }
+                else{
+                    event.getTableColumn().setVisible(false);
+                    event.getTableColumn().setVisible(true);
+
+
+                }
+
+
+            }
+        });
+        whiteboard.setCellValueFactory(new PropertyValueFactory<Room,Integer>("hasWhiteboard"));
+        whiteboard.setPrefWidth(10.0);
+
+        TableColumn coffeMachine = new TableColumn("CoffeeMachine");
+        coffeMachine.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        coffeMachine.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Room,Integer>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Room,Integer> event) {
+
+                if(event.getNewValue() == 1 || event.getNewValue() == 0) {
+
+                    System.out.println("is 1 or 0");
+                    Room updatedRoom = event.getTableView().getItems().get(event.getTablePosition()
+                            .getRow());
+                    updatedRoom.setHasCoffeeMachine(event.getNewValue());
+                    if (!updatedRooms.contains(updatedRoom)) {
+                        System.out.println("in first if,\n this is updatedRooms");
+                        updatedRooms.add(updatedRoom);
+                        System.out.println(updatedRooms.toString());
+                    } else {
+                        System.out.println("in else,\n this is updatedRooms");
+                        System.out.println(updatedRooms.toString());
+                    }
+                    System.out.println("this is tableview ArrayList:");
+                    System.out.println(tableViewData.toString());
+                }
+                else{
+                    event.getTableColumn().setVisible(false);
+                    event.getTableColumn().setVisible(true);
+
+
+                }
+            }
+        });
+        coffeMachine.setCellValueFactory(new PropertyValueFactory<Room,Integer>("hasCoffeeMachine"));
+        coffeMachine.setPrefWidth(10.0);
+
+        TableColumn buttons = new TableColumn();
+        buttons.setSortable(false);
+        buttons.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Room,Boolean>,
+                ObservableValue<Boolean>>() {
+            @Override
+            public ObservableValue<Boolean> call(TableColumn.CellDataFeatures<Room,Boolean> param) {
+                return new SimpleBooleanProperty(param.getValue() != null);
+            }
+        });
+
+        buttons.setCellFactory(new Callback<TableColumn<Room,Boolean>, TableCell<Room,Boolean>>() {
+            @Override
+            public TableCell<Room,Boolean> call(TableColumn<Room,Boolean> param) {
+
+
+                return new ButtonCell();
+            }
+        });
+
+        table.getColumns().addAll(roomid,location,size,projector,whiteboard,coffeMachine,buttons);
+//        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        table.setColumnResizePolicy(new Callback<TableView.ResizeFeatures, Boolean>() {
+            @Override
+            public Boolean call(TableView.ResizeFeatures param) {
+                param.getColumn().prefWidthProperty().bind(table.widthProperty().multiply(0.1));
+                return true;
+            }
+        });
+        table.setEditable(true);
+
 
 
     }
