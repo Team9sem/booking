f911672b494dca92a838e3d52018711bc4664ea1
Nima Fard <nimafard@Nimas-MacBook-Pro.local>
2015-11-29 15:51:27 +0100

    added variables to profile controller

--- src/com/team9/bookingsystem/Controllers/ProfileController.java
+++ src/com/team9/bookingsystem/Controllers/ProfileController.java
@@ -1,5 +1,30 @@
 package com.team9.bookingsystem.Controllers;
 
-public class ProfileController {
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 
+public class ProfileController {
+	
+	@FXML AnchorPane UserProfileAnchor;
+	@FXML TableView<?> currentBookings;
+	@FXML TableView<?> bookingHistory;
+	@FXML Label profileTitle;
+	@FXML Label userInfo;
+	@FXML Label UserName;
+	@FXML Label passWord;
+	@FXML Label firstName;
+	@FXML Label lastName;
+	@FXML Label adress;
+	@FXML Label ssd;
+	@FXML Label zipCode;
+	
+	public void showCurrentBookings(){
+		
+	}
+	
 }
+
+ 
